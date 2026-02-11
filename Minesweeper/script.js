5182ffce0b91ae3448f929546f67f3d1bd88984e
anus22hka <piyxmaru@gmail.com>
2026-02-11 21:37:07 +0530

    Update script.js

--- Minesweeper/script.js
+++ Minesweeper/script.js
@@ -1,1 +1,9 @@
-jojrifj nvndjskv
+document.addEventListener('DOMContentLoaded', function() {
+   const grid=document.querySelector('.grid')
+   const flagsleft=document.querySelector('#flags-left')
+   const result = document.querySelector('#result')
+    const width=10
+    let BombAmount=20
+    let flags=0
+    let squares=[]
+    let isGameOver = false
