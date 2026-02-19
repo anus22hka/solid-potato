b4b33c12c34ba602234f00345a980a1c753b83cb
anus22hka <piyxmaru@gmail.com>
2026-02-19 12:36:52 +0530

    Click function defined

--- Minesweeper/script.js
+++ Minesweeper/script.js
@@ -80,3 +80,28 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
    }
+
+   function click(square) {
+    console.log(square)
+    if (isGameOver || square.classList.contains('checked') || square.classList.contains('flag')) return
+
+    if(square.classList.contains('bomb')) {
+        gameOver()
+    }
+    else{
+        let total=square.getAttribute('data')
+        if(total != 0) {
+            
+            if(total == 1) square.classList.add('one')
+            if(total == 2) square.classList.add('two')
+            if(total == 3) square.classList.add('three')
+            if(total == 4) square.classList.add('four')
+                square.innerHTML=total
+            return
+        
+        }
+        checkSquare(square)
+    }
+    square.classList.add('checked')
+        
+   }
